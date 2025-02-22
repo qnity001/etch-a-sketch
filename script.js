64e2431231eff9e99aac67bf456de329614ab0c8
Aarya <aishwaryam.aarya@gmail.com>
2025-02-22 22:42:49 +0530

    Title: Add 256 divs
    Body: Add 256 divs to the gameContainer in script.js
    and it appears on the html page as "x"

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Somehow create 256 divs
 // For loop?
 //  Create a matrix of 16 x 16
-
+n = 16
 // Go into the game-container div
-const gameContainer = document.querySelector("game-container")
+const gameContainer = document.querySelector(".game-container");
 
 for (let i = 0; i < n; i++){
     for (let j = 0; j < n; j++){
         // Create div
-        const div = document.createElement("div")
-        gameContainer.appendChild(div)
+        const div = document.createElement("div");
+        div.textContent = "x";
+        gameContainer.appendChild(div);
     }
 }
