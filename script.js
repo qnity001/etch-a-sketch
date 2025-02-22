6d1817ad5c4707cb8afa999532d8d4da288a8340
Aarya <aishwaryam.aarya@gmail.com>
2025-02-22 22:35:39 +0530

    Find the game container div in script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const gameContainer = document.querySelector(".game-container");
-n = 16*16;
-
-function createBoxes (n) {
-    for (let i = 0; i < n; i++) {
-        gameContainer.appendChild(document.createElement("div"));
-    }
-}
