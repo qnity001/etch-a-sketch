1a91be0d1ccb5264b9b1f91bcae4da53bccfbf4a
Aarya <aishwaryam.aarya@gmail.com>
2025-02-23 11:19:30 +0530

    Add div to class 'pixel' in script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Somehow create 256 divs
-// For loop?
 //  Create a matrix of 16 x 16
 n = 16
+
 // Go into the game-container div
 const gameContainer = document.querySelector(".game-container");
 
@@ -9,6 +8,7 @@ for (let i = 0; i < n; i++){
     for (let j = 0; j < n; j++){
         // Create div
         const div = document.createElement("div");
+        div.classList.add("pixel")
         div.setAttribute("style", "width: 40px; height: 40px; border: 1px solid grey; box-sizing: border-box");
         gameContainer.appendChild(div);
     }
