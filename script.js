3af2e30870c98e9e1f1ac008db53c459e1aa6b06
Aarya <aishwaryam.aarya@gmail.com>
2025-02-23 10:27:02 +0530

    Remove background colour of individual box in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (let i = 0; i < n; i++){
     for (let j = 0; j < n; j++){
         // Create div
         const div = document.createElement("div");
-        div.setAttribute("style", "background: yellow; width: 60px; height: 60px; border: 1px solid grey; box-sizing: border-box");
+        div.setAttribute("style", "width: 60px; height: 60px; border: 1px solid grey; box-sizing: border-box");
         gameContainer.appendChild(div);
     }
 }
